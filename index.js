318954138d9d98e7bd90a37dada7a920e4cc492c
Aryaman Singh <arya.ribharya@gmail.com>
2021-07-30 19:31:40 +0530

    v1.0giveupkardungabroab

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ const headers = {
        
     Authorization:process.env.GITHUB_TOKEN,
   };
+  
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 app.get("/",async(req,res)=>{
     res.send(headers);
 })
